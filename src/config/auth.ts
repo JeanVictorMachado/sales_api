c21f2767e8fe30677072e27e7217852127556cdf
JeanVictorMachado <jeanvictor.arq@gmail.com>
2021-10-19 20:52:05 -0300

    feat: creating authenticated middleware

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: '0b128bc6e547ca715464359eccf6ba83',
+    expiresIn: '1d',
+  },
+};
--- src/modules/users/controllers/UsersController.ts
+++ src/modules/users/controllers/UsersController.ts
@@ -3,7 +3,7 @@ import CreateUserService from '../services/CreateUserService';
 import ListUserService from '../services/ListUserService';
 
 export default class UsersController {
-  public async index(request: Request, response: Response): Promise<Response> {
+  public async index(_request: Request, response: Response): Promise<Response> {
     const listUsers = new ListUserService();
 
     const users = await listUsers.execute();
